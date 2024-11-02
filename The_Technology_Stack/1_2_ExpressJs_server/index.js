23fc756fa61d0036925ac6dbd311c28831b0b3ba
TulioG_Projects <tuliog_public@proton.me>
2024-11-02 20:19:43 +0800

    finished chapter 2

--- The_Technology_Stack/1_2_ExpressJs_server/index.js
+++ The_Technology_Stack/1_2_ExpressJs_server/index.js
@@ -3,15 +3,33 @@
 
 // Notice that this is using the 'require' syntax, but I believe
 //  the book will eventually transition to the 'import syntax"
-const express = require("express");
+// The below was eventually replaced with the 'import' syntax
+// const express = require("express");
+
+import { routeHello, routeAPINames} from "./routes.js"
+import express from "express"
 
 const server = express();
 const port = 3000;
 
 // Respond to a GET request on /hello with "Hello World!"
 server.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  const response = routeHello(req, res);
+  res.send(response);
 });
+
+// adding a route
+server.get('/api/names', async (req, res) => {
+  let response;
+
+  try {
+    response = await routeAPINames(req, res);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.send(response);
+})
 
 // Turns on the port on localhost:3000/
 server.listen(port, () => {
