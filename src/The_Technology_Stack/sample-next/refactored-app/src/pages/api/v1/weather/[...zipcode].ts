80fe0008c3c355efb74740c89e4c86ea0cc66eb2
TulioG_Projects <tuliog_public@proton.me>
2024-12-06 00:14:06 +0800

    continued ch 7

--- src/The_Technology_Stack/sample-next/refactored-app/src/pages/api/v1/weather/[...zipcode].ts
+++ src/The_Technology_Stack/sample-next/refactored-app/src/pages/api/v1/weather/[...zipcode].ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../../../../middleware/db-connect";
+import { findByZip } from "../../../../../mongoose/weather/services";
 
 type WeatherDetailType = {
   zipcode: string;
@@ -11,7 +13,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<NextApiResponse<WeatherDetailType> | void> {
-  return res
-    .status(200)
-    .json({ zipcode: req.query.zipcode, weather: "sunny", temp: 35 });
+  dbConnect();
+
+  const data = await findByZip(req.query.zipcode as string);
+  return res.status(200).json(data);
 }
